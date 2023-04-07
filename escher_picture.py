7e7213780a092459e0d2ba50cc128750a3312549
Harry Bray <harry.bray23@gmail.com>
2023-04-07 10:00:22 -0400

    poincare_orthocircle takes in complex inputs now

--- escher_picture.py
+++ escher_picture.py
@@ -19,8 +19,24 @@ def mobius_reflection(a,b):
 
 A=mobius_reflection(-2.414,.414)
 B=mobius_reflection(-.613,3.573)
-Ainv=A
-Binv=B
+
+#print(A)
+# this is the standard conformal map sending the disk to the upper half
+# plane
+#a= complex(0,-1)
+#a=0-1j
+#print(a)
+J=np.array([[0-1j,1+0j],[1+0j,0-1j]])
+Jinv=np.linalg.inv(J)
+
+A=np.dot(A,J)
+A=np.dot(Jinv,A)
+B=np.dot(B,J)
+B=np.dot(Jinv,B)
+#print(J)
+#print(np.dot(J,A))
+
+#print(A)
         
 
 ### function for matrix product
@@ -35,65 +51,73 @@ def mp(list_of_matrices):
 ### this function is more general than needed for this picture but maybe it
 ### will be useful later?
 
-def mobius_transformation(M,v):
+#def mobius_transformation(M,v):
+#    # M is a matrix which represents a mobius transformation
+#    # v is a vector in R^2 which will be our model for the complex plane
+#    # via the bijection [x,y] <-> x+iy
+#    # output will be a vector
+#    a=M[0][0]
+#    b=M[0][1]
+#    c=M[1][0]
+#    d=M[1][1]
+#    x=v[0]
+#    y=v[1]
+#    numerator_real_part = (a*x+b)*(c*x+d)+a*c*y*y
+#    numerator_imaginary_part = (a*d-b*c-a*c*x)*y
+#    denominator = (c*x+d)**2+(c*y)**2
+##    if denominator!=0:
+#    return [numerator_real_part/denominator,numerator_imaginary_part/denominator]
+##    else:
+
+def mobius_transformation(M,z):
     # M is a matrix which represents a mobius transformation
-    # v is a vector in R^2 which will be our model for the complex plane
-    # via the bijection [x,y] <-> x+iy
-    # output will be a vector
+    # z is a complex number
     a=M[0][0]
     b=M[0][1]
     c=M[1][0]
     d=M[1][1]
-    x=v[0]
-    y=v[1]
-    numerator_real_part = (a*x+b)*(c*x+d)+a*c*y*y
-    numerator_imaginary_part = (a*d-b*c-a*c*x)*y
-    denominator = (c*x+d)**2+(c*y)**2
-#    if denominator!=0:
-    return [numerator_real_part/denominator,numerator_imaginary_part/denominator]
+
+    return (a*z+b)/(c*z+d)
+
+
+
+
+# this function seems unnecessary
+
+def mobius_of_geodesic(M,v,w,height=4):
+    # M is a matrix which represents a mobius transformation
+    # v,w are distinct vectors that represent the endpoints of the initial
+    # geodesic in the boundary of the Poincare disk
+    # output will be a tuple of distinct vectors representing the image
+    # geodesic
+
+
+#    r_hor=r[0]
+#    r_vert=r[1]
+#    t_hor=t[0]
+#    t_vert=t[1]
+#    a=M[0][0]
+#    b=M[0][1]
+#    c=M[1][0]
+#    d=M[1][1]
+
+    endpoint1=mobius_transformation(M,v)
+    endpoint2=mobius_transformation(M,w)
+#    if r_hor==t_hor:
+#        if c==0:
+#            endpoint1=mobius_transformation(M,[r_hor,0])
+#            endpoint2=mobius_transformation(M,[r_hor,height])
+#        else:
+#            if c*r_hor+d==0:
+#                endpoint1=[a/c,height]#mobius_transformation(M,[r_hor,0])
+#                endpoint2=[a/c,0.]
+#            else:
+#                endpoint1=mobius_transformation(M,[r_hor,0])
+#                endpoint2=[a/c,0.]
 #    else:
-
-# need to upgrade this function so that it can also act on vertical
-# geodesics
-
-def mobius_of_geodesic(M,r,t,height=4):
-    # M is a matrix which represents a mobius transformation
-    # r,t are distinct vectors with non-negative vertical coordinate.
-    # the function should calculate the geodesic from a to b and return the
-    # pair of endpoints at infinity of the geodesic - except if the
-    # geodesic is a vertical line, then one of the endpoints will have
-    # vertical coordinate equal to the height (this will represent
-    # infinity)
-
-
-    # for the moment I am going to assume that if a,b have different
-    # horizontal coordinates, then a,b are both on
-    # the real axis. It's much simpler. In the long run, would be
-    # nice to have a function which takes any two points and
-    # calculates their geodesic orthocircle
-    r_hor=r[0]
-    r_vert=r[1]
-    t_hor=t[0]
-    t_vert=t[1]
-    a=M[0][0]
-    b=M[0][1]
-    c=M[1][0]
-    d=M[1][1]
-    if r_hor==t_hor:
-        if c==0:
-            endpoint1=mobius_transformation(M,[r_hor,0])
-            endpoint2=mobius_transformation(M,[r_hor,height])
-        else:
-            if c*r_hor+d==0:
-                endpoint1=[a/c,height]#mobius_transformation(M,[r_hor,0])
-                endpoint2=[a/c,0.]
-            else:
-                endpoint1=mobius_transformation(M,[r_hor,0])
-                endpoint2=[a/c,0.]
-    else:
-        endpoint1=mobius_transformation(M,r)
-        endpoint2=mobius_transformation(M,t)
-    return [endpoint1,endpoint2]
+#        endpoint1=mobius_transformation(M,r)
+#        endpoint2=mobius_transformation(M,t)
+    return (endpoint1,endpoint2)
 
 
 #print(mobius_of_geodesic(mp([A,B]),[1,3],[1,0]))
@@ -171,58 +195,65 @@ def eucl_dist(v,w):
 
 
 def poincare_orthocircle(v,w,default_color='black',default_linewidth=1):
-    #v, w are lists with two coordinates
-    v_hor=v[0]
-    v_vert=v[1]
-    w_hor=w[0]
-    w_vert=w[1]
+    # z,w are complex number
 
+#    v_hor=v[0]
+#    v_vert=v[1]
+#    w_hor=w[0]
+#    w_vert=w[1]
+    v_hor=v.real
+    v_vert=v.imag
+    w_hor=w.real
+    w_vert=w.imag
+
+    v_vec=[v_hor,v_vert]
+    w_vec=[w_hor,w_vert]
 
     if v_vert!=0:
-        v_slope=slope_of_circ(v)
+        v_slope=slope_of_circ(v_vec)
         v_height=-v_hor*v_slope+v_vert
     if w_vert!=0:
-        w_slope=slope_of_circ(w)
+        w_slope=slope_of_circ(w_vec)
         w_height=-w_hor*w_slope+w_vert
 
     if v_vert==0: 
         if w_vert==0:
-            draw_circle=draw_lines([v,w],default_color,default_linewidth)
-            print(v,w)
+            draw_circle=draw_lines([v_vec,w_vec],default_color,default_linewidth)
+#            print(v,w)
         elif w_hor==0: 
             center=(v_hor,w_vert)
-            radius=eucl_dist(v,center)
+            radius=eucl_dist(v_vec,center)
             draw_circle= patches.Arc(center, radius, radius, 0.0, 0.0, 360, color=default_color, linewidth=default_linewidth)
             draw_circle=patches.Circle(center,radius,color=default_color,linewidth=default_linewidth,fill=False)
         else: 
             center=(v_hor, w_slope*v_hor+w_height)
-            radius=eucl_dist(v,center)
+            radius=eucl_dist(v_vec,center)
             draw_circle= patches.Arc(center, radius, radius, 0.0, 0.0, 360, color=default_color, linewidth=default_linewidth)
             
     elif w_vert==0:
         if v_hor==0:
             center=(w_hor,v_vert)
-            radius = eucl_dist(v,center)
+            radius = eucl_dist(v_vec,center)
             draw_circle= patches.Arc(center, radius, radius, 0.0, 0.0, 360, color=default_color, linewidth=default_linewidth)
             draw_circle=patches.Circle(center,radius,color=default_color,linewidth=default_linewidth,fill=False)
         else: 
             center=(w_hor, v_slope*w_hor+v_height)
-            radius=eucl_dist(v,center)
+            radius=eucl_dist(v_vec,center)
             draw_circle= patches.Arc(center, radius, radius, 0.0, 0.0, 360, color=default_color, linewidth=default_linewidth)
             draw_circle=patches.Circle(center,radius,color=default_color,linewidth=default_linewidth,fill=False)
     else: 
         if v_slope==w_slope:
-            draw_circle=draw_lines([v,w],default_color,default_linewidth)
+            draw_circle=draw_lines([v_vec,w_vec],default_color,default_linewidth)
         else: 
             center=intersect_lines(v_slope,v_height,w_slope,w_height)
-            radius=eucl_dist(center,w)
+            radius=eucl_dist(center,w_vec)
             draw_circle= patches.Arc(center, radius, radius, 0.0, 0.0, 360, color=default_color, linewidth=default_linewidth)
             draw_circle=patches.Circle(center,radius,color=default_color,linewidth=default_linewidth,fill=False)
             draw_circle=ax.add_patch(draw_circle)
 
-            print(v_slope,v_height,w_slope,w_height)
-            print(center)
-            print(radius)
+#            print(v_slope,v_height,w_slope,w_height)
+#            print(center)
+#            print(radius)
 
     
     return draw_circle
@@ -260,40 +291,56 @@ lw=.5
 
 ax.add_patch(patches.Circle( (0,0),1,fill=False))
 
-A=mobius_reflection(-2.414,.414)
-B=mobius_reflection(-.613,3.573)
+#A=mobius_reflection(-2.414,.414)
+#B=mobius_reflection(-.613,3.573)
 #A=[[0,1],[1,0]]
 
-n=2
-#for G in group_elts(n,[A,B]):
-##    draw_geodesic(mobius_of_geodesic(G, [-2.414,.414],[-.613,3.573]),lw)
-#    draw_geodesic(mobius_of_geodesic(G, [0,4],[0,0]),lw)
-
-#print(group_elts(n))
-
-#draw_geodesic(mobius_of_geodesic(A, [0,4],[0,0]))
-#draw_geodesic(mobius_of_geodesic(Ainv, [0,4],[0,0]))
-#draw_geodesic(mobius_of_geodesic(B, [0,4],[0,0]))
 
 
-#draw_geodesic([[1,4],[1,0]])
-#draw_geodesic([[2,0],[1,0]])
+#v=(sqrt(2)/2,sqrt(2)/2)
 
-v=[sqrt(2)/2,sqrt(2)/2]
-w=[1/2,sqrt(3)/2]
 
-poincare_orthocircle(v,w)
-
-v=[sqrt(2)/2,sqrt(2)/2]
-w=[0,1]
+#z= complex(sqrt(2)/2,sqrt(2)/2)
+#print(z)
 #
+#print(mobius_transformation(A,z))
+#
+#print(mobius_transformation(A,-1j))
+#print(mobius_transformation(A,-1))
+
+#n=2
+#for G in group_elts(n,[A,B]):
+    # apply G to v,w
+
+#w=1
+
+#Az=mobius_transformation(A,z)
+#Aw=mobius_transformation(A,w)
+
+
+
+
+A=[[0,1],[1,0]]
+v=complex(sqrt(2)/2,sqrt(2)/2) 
+w=1j
+
+#print()
+#print(v.real)
+#
+
 poincare_orthocircle(v,w)
 
 
+poincare_orthocircle(mobius_transformation(A,v),mobius_transformation(A,w))
+
+#w=complex(1/2,-sqrt(3)/2)
+#
+##poincare_orthocircle(v,w)
 
 
-poincare_orthocircle([1,0],[-1,0])
-poincare_orthocircle([0,1],[0,-1])
+
+#poincare_orthocircle([1,0],[-1,0])
+#poincare_orthocircle([0,1],[0,-1])
 
 axes = plt.gca()
 axes.set_xlim([-1.1,1.1])
